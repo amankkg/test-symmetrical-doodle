67d75bae28e40800ede85f9a465698e7da0a840c
Aman Kubanychbek <amankubanychbek@gmail.com>
2019-08-20 08:04:49 +0000

    add color calculation for percentage

--- src/table-cell.js
+++ src/table-cell.js
@@ -4,9 +4,14 @@ import {Slot} from './slot'
 
 const total = 12
 const slots = Array.from({length: total}, (_, i) => `${i + total - 1}:00`)
-const percentage = actual => (actual / total) * 100 // TODO: add colors (from 1% green to 100% red)
+const percentage = actual => (actual / total) * 100
+// get hue value in range from green (120) to red (0)
+const getHue = percent => ((100 - percent) * Math.abs(120 - 0)) / 100
 
 export const TableCell = ({data}) => {
+  const percent = percentage(data.length)
+  const color = percent === 0 ? 'unset' : `hsl(${getHue(percent)}, 75%, 50%)`
+
   const renderSlot = time => (
     <Slot key={time} time={time} busy={data.includes(time)} />
   )
@@ -14,7 +19,7 @@ export const TableCell = ({data}) => {
   return (
     <div className="cell">
       <div className="cell-header">
-        <span>{Math.round(percentage(data.length))}</span>
+        <span style={{color}}>{Math.round(percent)}</span>
         <span>%</span>
         <span tabIndex="0">•••</span>
       </div>
